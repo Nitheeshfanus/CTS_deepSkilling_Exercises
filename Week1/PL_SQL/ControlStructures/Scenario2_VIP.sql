3e8158b630a0d16b138819236b10869466b124f9
itsh <itshfanus@gmail.com>
2026-06-21 23:04:20 +0530

    JunitExercises, PL_SQL

---
+++ Week1/PL_SQL/ControlStructures/Scenario2_VIP.sql
@@ -0,0 +1,16 @@
+DECLARE
+BEGIN
+    FOR cust IN (
+        SELECT CustomerID
+        FROM Customers
+        WHERE Balance > 10000
+    )
+    LOOP
+        UPDATE Customers
+        SET IsVIP = 'TRUE'
+        WHERE CustomerID = cust.CustomerID;
+    END LOOP;
+
+    COMMIT;
+END;
+/
