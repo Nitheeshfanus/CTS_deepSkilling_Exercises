7c988a7703a9829f66944c2e122cc1ab42813f7a
itsh <itshfanus@gmail.com>
2026-06-22 22:08:50 +0530

    TDD JUnit  testing

---
+++ Week1/JUnitExercises/EX_1_Mocking_And_Stubbing/src/test/java/MyServiceTest.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+public class MyServiceTest {
+
+    @Test
+    public void testExternalApi() {
+
+        // Create Mock
+        ExternalApi mockApi =
+                Mockito.mock(ExternalApi.class);
+
+        // Stub Method
+        when(mockApi.getData())
+                .thenReturn("Mock Data");
+
+        // Inject Mock
+        MyService service =
+                new MyService(mockApi);
+
+        // Execute
+        String result =
+                service.fetchData();
+
+        // Verify
+        assertEquals("Mock Data", result);
+    }
+}
